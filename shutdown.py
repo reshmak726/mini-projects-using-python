9c84688876bd465d15c3c3182a1320aa1a73a3f4
Reshma Khan <reshmak726@gmail.com>
2022-04-01 12:09:02 +0530

    added functionality

--- shutdown.py
+++ shutdown.py
@@ -1,22 +1,31 @@
 import imp
 from tkinter import *
 import os
+def restart():
+    os.system("shutdown /r/t 1")
+def restart_time():
+    os.system("shutdown /r/t 20")
+def logout():
+    os.system("shutdown -1")
+def shutdown():
+    os.system("shutdown /s /t 1")
+
 
 st=Tk()
 st.title("Shutdown app")
 st.geometry("500x500")
 st.config(bg="Blue")
 
-r_button = Button(st,text="Restart",font=("Time New Roman",30,"bold"),relief=RAISED,cursor="plus")
+r_button = Button(st,text="Restart",font=("Time New Roman",30,"bold"),relief=RAISED,cursor="plus",command=restart)
 r_button.place(x=150,y=60,height=50,width=200)
 
-rt_button = Button(st,text="Restart Time",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+rt_button = Button(st,text="Restart Time",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=restart_time)
 rt_button.place(x=150,y=170,height=50,width=200)
 
-lg_button = Button(st,text="Log-out",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+lg_button = Button(st,text="Log-out",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=logout)
 lg_button.place(x=150,y=270,height=50,width=200)
 
-st_button = Button(st,text= "Shut-down",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus")
+st_button = Button(st,text= "Shut-down",font=("Time New Roman",20,"bold"),relief=RAISED,cursor="plus",command=shutdown)
 st_button.place(x=150,y=370,height=50,width=200)
 
 st.mainloop()
