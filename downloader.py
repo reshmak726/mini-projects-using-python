357dca25def0dba685e1a1fee33013187e11b658
Reshma Khan <reshmak726@gmail.com>
2022-04-02 14:36:25 +0530

    youtube video and audio downloder code added

---
+++ downloader.py
@@ -0,0 +1,18 @@
+# from pytube import YouTube
+
+# link = "https://www.youtube.com/watch?v=NwdQx2P_ytk"
+# youtube1 = YouTube(link)
+
+# # print(youtube1.title)
+# # print(youtube1.thumbnail_url)
+# video = youtube1.streams.all()
+# # video = youtube1.streams.filter(only_audio=True)  only audio will be downloaded using this
+# vid = list(enumerate(video))
+# for i in vid:
+#     print(i)
+# print()
+# strm = int(input("enter : "))
+# video[strm].download()
+# print("successfully downloaded the video")
+
+
