22030107fbe4aa0cd31125e484b376584b5232cd
Reshma Khan <reshmak726@gmail.com>
2022-04-08 20:07:56 +0530

    some more functions

--- voiceassist.py
+++ voiceassist.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import pyttsx3
 import speech_recognition as sr
 import webbrowser
@@ -15,8 +16,10 @@ def sptext():
             print("recognizing...")
             data = recognizer.recognize_google(audio)
             print(data)
+            return data
         except sr.UnknownValueError:
             print("not understanding message")
+# sptext()            
 
 def speechtx(x):
     engine = pyttsx3.init()
@@ -27,7 +30,27 @@ def speechtx(x):
     engine.say(x)
     engine.runAndWait()
 
-speechtx("helo and welcome to mumbai india")
+# speechtx("helo and welcome to mumbai india")
 
 
+if __name__ == '__main__':
 
+    # if sptext().lower() =="hello world":
+       data1=sptext().lower()
+
+       if "your name" in data1:
+           name = "my name is henry"
+           speechtx(name)
+       elif "old" in data1:
+            age = "i am 22 years old"
+            speechtx(age) 
+       elif ' time' in data1:
+           time = datetime.datetime.now().strftime("%I%M%p")
+           speechtx(time)
+       elif 'youtube' in data1:
+           webbrowser.open("https://www.youtube.com/watch?v=OKuiyX5k6zg")
+
+       elif "joke" in data1:
+           joke_1 = pyjokes.get_joke(language="en" , category="neutral")
+           print(joke_1)
+           speechtx(joke_1)
